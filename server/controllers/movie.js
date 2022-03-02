bc64ec18d2e86838c475abca469db5374ef2680a
Çlirim Krasniqi <qliraa19@gmail.com>
2022-03-02 02:06:30 +0100

    removed the limit per movies

--- server/controllers/movie.js
+++ server/controllers/movie.js
@@ -152,13 +152,11 @@ exports.update = async (req, res) => {
 exports.list = async (req, res) => {
   let order = req.query.order ? req.query.order : "asc";
   let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
   Movie.find()
     .select("-photo")
     .populate("category")
     .sort([[sortBy, order]])
-    .limit(limit)
     .exec((err, data) => {
       if (err) {
         return res.status(400).json({
